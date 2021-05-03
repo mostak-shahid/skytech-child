938e1ef8be799aa9400032284053a39f04569de3
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-05-03 10:56:44 +0600

    1.0.2

--- carbon-fields.php
+++ carbon-fields.php
@@ -315,9 +315,9 @@ function crb_attach_theme_options() {
                         <?php endif?>
                         <div class="line-filter bg-theme p-30">
                             <div class="filter-content">
-                                <h3 class="mb-10 text-capitalize mos-services-block-title"> <a class="text-white" href="<?php echo ($slide['btn-url'])?$slide['btn-url']:'#' ?>"><?php echo do_shortcode($slide['title']) ?></a></h3>
+                                <h3 class="mb-0 text-capitalize mos-services-block-title"> <a class="text-white" href="<?php echo ($slide['btn-url'])?$slide['btn-url']:'#' ?>"><?php echo do_shortcode($slide['title']) ?></a></h3>
                                 <?php if ($slide['btn-title']) : ?> 
-                                <a href="<?php echo ($slide['btn-url'])?$slide['btn-url']:'#' ?>" class="site-button-link text-white"><?php echo $slide['btn-title'] ?></a>
+                                <a href="<?php echo ($slide['btn-url'])?$slide['btn-url']:'#' ?>" class="site-button-link mt-10 text-white"><?php echo $slide['btn-title'] ?></a>
                                 <?php endif;?>
                             </div>
                         </div>
